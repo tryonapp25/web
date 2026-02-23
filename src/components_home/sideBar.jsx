c368c48ff189bf33ae5af099591be33606040c03
minhlu14206 <minhlu14206@gmail.com>
2026-02-23 14:17:01 +0100

    update

--- src/components_home/sideBar.jsx
+++ src/components_home/sideBar.jsx
@@ -76,9 +76,15 @@ export default function Sidebar({items}) {
             active={isActive("/admin")}
             onPress={() => navigate("/admin")}
           />
-        )
-          
-        }
+        )}
+        {publicUser?.role === "admin" || publicUser?.role === "business" && (
+          <Item
+            icon={Shield}
+            label="Business"
+            active={isActive("/business")}
+            onPress={() => navigate("/business")}
+          />
+        )}
       </div>
 
       <div className={styles.divider} />
