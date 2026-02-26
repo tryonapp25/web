c75c8989935f8fc90e0fad32224afb4688155637
minhlu14206 <minhlu14206@gmail.com>
2026-02-26 11:43:48 +0100

    update

--- src/utils/renderProductionConnection.js
+++ src/utils/renderProductionConnection.js
@@ -1,16 +1,23 @@
 import { getFeatureFlags } from "../featureFlags/featureFlags";
+import http from "../http/http";
 
+const publicCode = new URLSearchParams(window.location.search).get("public");
 
 const getFlag = async () => {
     const flag = await getFeatureFlags("ORDER_FEATURE");
     return flag
 }
 
+const getEnabledBusinessOrderOnlineFeature = async () => {
+    return true;
+}
 
 export default async function connectToSocket(connectGuest, connected, setSocketEnabled) {
-    const orderFeatureEnabled = await getFlag();
-    setSocketEnabled(orderFeatureEnabled);
-    if(!orderFeatureEnabled) return;
+    const orderFlagEnabled = await getFlag();
+    const businessOrderOnlineEnabled = await getEnabledBusinessOrderOnlineFeature();
+
+    setSocketEnabled(orderFlagEnabled);
+    if(!orderFlagEnabled && !businessOrderOnlineEnabled) return;
     if(connected) return; // already connected
         (async () => {
         try {
