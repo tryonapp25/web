009de4e30c487b6d9c0994aded469790c5046301
minhlu14206 <minhlu14206@gmail.com>
2026-03-07 00:35:32 +0100

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCmExZLlpyDs0IBICIcOiAOccjnX0Ololo",
+  authDomain: "tryon-308c9.firebaseapp.com",
+  projectId: "tryon-308c9",
+  storageBucket: "tryon-308c9.firebasestorage.app",
+  messagingSenderId: "998228351886",
+  appId: "1:998228351886:web:27a9c2568077ff2b2522df",
 });
 
 const messaging = firebase.messaging();
