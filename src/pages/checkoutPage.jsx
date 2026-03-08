59bdbf93cf5ff5c08fe7f39569797c643fa8e437
minhlu14206 <minhlu14206@gmail.com>
2026-03-08 22:11:05 +0100

    update

--- src/pages/checkoutPage.jsx
+++ src/pages/checkoutPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import http_order from "../http/http_order";
 import httpMessage from "../http/httpMessage";
-import { useNavigate, useParams,useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import CheckoutForm from "../components/checkOutForm";
 import FlashMessage from "../components/flashMessage";
 import defaultMessage from "../utils/defaultMessage";
@@ -9,106 +9,121 @@ import LoadingModal from "../components/loading";
 import PopupMessage from "../components/popupMessage";
 
 export default function CheckoutPage() {
-    const navigate = useNavigate();
-    const fetchedRef = useRef(false);
-    const [searchParams] = useSearchParams();
-    const orderId  = searchParams.get("orderId");
-    const paymentIntentId = searchParams.get("paymentIntentId");
+  const navigate = useNavigate();
+  const fetchedRef = useRef(false);
+  const [searchParams] = useSearchParams();
+  const orderId = searchParams.get("orderId");
+  const paymentIntentId = searchParams.get("paymentIntentId");
 
-    const [message, setMessage] = useState(defaultMessage);
-    const [loading, setLoading] = useState(false);
-    const [paymentIntentData, setPaymentIntentData] = useState(null);
-    const [openCheckout, setOpenCheckout] = useState(false);
-    const [paymentSuccess, setPaymentSuccess] = useState(false);
+  const [message, setMessage] = useState(defaultMessage);
+  const [loading, setLoading] = useState(false);
+  const [paymentIntentData, setPaymentIntentData] = useState(null);
+  const [openCheckout, setOpenCheckout] = useState(false);
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
-    useEffect(() => {
-        if (!paymentIntentId) {
-            setMessage({
-                visible: true,
-                type: "error",
-                msg: "No payment intent ID provided."
-            });
-            return;
-        }
-
-        if (fetchedRef.current) return;
-        fetchedRef.current = true;
-
-        handleGetPayment(paymentIntentId);
-    }, [paymentIntentId, navigate]);
-
-    const handleGetPayment = async (paymentIntentId) => {
-        try {
-            setLoading(true);
-            const res = await http_order.get(`/order/payment/intent/${paymentIntentId}`);
-
-            if (res.data?.success) {
-                setPaymentIntentData(res.data.data);
-                if(res?.data?.data?.status === "succeeded") {
-                    setPaymentSuccess(true);
-                    return;
-                }
-                setOpenCheckout(true);
-            } else {
-                setMessage({
-                    visible: true,
-                    type: "error",
-                    msg: "Payment information not found."
-                });
-            }
-        } catch (err) {
-            setMessage({
-                visible: true,
-                type: "error",
-                msg: httpMessage(err) || "An error occurred while retrieving payment information."
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handlePaymentSuccess = async () => {
-        const newTab = window.open("", "_blank"); 
-        const receiptBaseUrl = import.meta.env.VITE_APP_PUBLIC_URL;
-
-        const url = `${receiptBaseUrl}/receipt/production?orderId=${orderId}`;
-        if (newTab) {
-            newTab.location.href = url;
-        }
-        Clear();
-    };
-
-    const Clear = () => {
-        setPaymentIntentData(prev => ({ ...prev, status: "succeeded" }));
-        setPaymentSuccess(false);
-        setMessage(defaultMessage);
+  useEffect(() => {
+    if (!paymentIntentId) {
+      setMessage({
+        visible: true,
+        type: "error",
+        msg: "No payment intent ID provided.",
+      });
+      return;
     }
 
-    return (
-        <div>
-            <CheckoutForm
-                open={openCheckout}
-                clientSecret={paymentIntentData?.clientSecret || ""}
-                orderId={orderId || null}
-                onClose={() => setOpenCheckout(false)}
-                onSuccess={handlePaymentSuccess}
-            />
+    if (fetchedRef.current) return;
+    fetchedRef.current = true;
 
-            <PopupMessage
-                open={paymentSuccess}
-                title="Payment Successful!"
-                text="Your payment was successful. You can view your receipt now."
-                onClose={() => setPaymentSuccess(false)}
-                type="success"
-            />
+    handleGetPayment(paymentIntentId);
+  }, [paymentIntentId]);
 
-            <FlashMessage
-                visible={message.visible}
-                type={message.type}
-                msg={message.msg}
-            />
+  const handleGetPayment = async (paymentIntentId) => {
+    try {
+      setLoading(true);
 
-            <LoadingModal open={loading} />
-        </div>
-    );
+      const res = await http_order.get(`/order/payment/intent/${paymentIntentId}`);
+
+      if (res.data?.success) {
+        setPaymentIntentData(res.data.data);
+
+        if (res.data?.data?.status === "succeeded") {
+          setPaymentSuccess(true);
+          return;
+        }
+
+        setOpenCheckout(true);
+      } else {
+        setMessage({
+          visible: true,
+          type: "error",
+          msg: "Payment information not found.",
+        });
+      }
+    } catch (err) {
+      setMessage({
+        visible: true,
+        type: "error",
+        msg: httpMessage(err) || "An error occurred while retrieving payment information.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handlePaymentSuccess = () => {
+    if (!orderId) {
+      setMessage({
+        visible: true,
+        type: "error",
+        msg: "Missing order ID.",
+      });
+      return;
+    }
+
+    const receiptBaseUrl = import.meta.env.VITE_APP_PUBLIC_URL;
+    const url = `${receiptBaseUrl}/receipt/production?orderId=${orderId}`;
+
+    Clear();
+
+    // More reliable than window.open() on mobile Safari
+    window.location.href = url;
+  };
+
+  const Clear = () => {
+    setPaymentIntentData((prev) => ({
+      ...prev,
+      status: "succeeded",
+    }));
+    setPaymentSuccess(false);
+    setMessage(defaultMessage);
+    setOpenCheckout(false);
+  };
+
+  return (
+    <div>
+      <CheckoutForm
+        open={openCheckout}
+        clientSecret={paymentIntentData?.clientSecret || ""}
+        orderId={orderId || null}
+        onClose={() => setOpenCheckout(false)}
+        onSuccess={handlePaymentSuccess}
+      />
+
+      <PopupMessage
+        open={paymentSuccess}
+        title="Payment Successful!"
+        text="Your payment was successful. You can view your receipt now."
+        onClose={() => setPaymentSuccess(false)}
+        type="success"
+      />
+
+      <FlashMessage
+        visible={message.visible}
+        type={message.type}
+        msg={message.msg}
+      />
+
+      <LoadingModal open={loading} />
+    </div>
+  );
 }
